e2506119a0854db243975ba6252355f1e26c0ab8
Ivan Ioffe <iiooffee0202@gmail.com>
2025-10-20 21:42:09 +0300

    refactor(shopping-list): remove NavController.navigateToShoppingList extension
    Removed navigateToShoppingList as it is no longer needed, since
    ShoppingListBaseRoute is set as startDestination in AppNavGraph
    when isLoggedIn is true, simplifying navigation logic.

--- feature/shopping-list/src/main/java/com/ioffeivan/feature/shopping_list/presentation/navigation/ShoppingListNavigation.kt
+++ feature/shopping-list/src/main/java/com/ioffeivan/feature/shopping_list/presentation/navigation/ShoppingListNavigation.kt
@@ -21,9 +21,6 @@ data object ShoppingListsRoute
 @Serializable
 data object CreateShoppingListRoute
 
-fun NavController.navigateToShoppingList(navOptions: NavOptions? = null) =
-    navigate(route = ShoppingListBaseRoute, navOptions = navOptions)
-
 fun NavController.navigateToCreateShoppingList(navOptions: NavOptions? = null) =
     navigate(route = CreateShoppingListRoute, navOptions = navOptions)
 
